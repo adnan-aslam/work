33abfec9e0d93ab695101da474a83eeba03ecbea
Adnan Aslam <aaslam@talabatoutlook.onmicrosoft.com>
2019-01-27 23:27:08 +0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        string name;
+        string email;
+        int age;
+
+
+        string getName()
+        {
+            return name;
+
+        }
+    }
+}
